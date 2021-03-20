d807efbc60a2d1510b320f62a40836c3dc6e6d22
N15H4N7 <ngupta_be19@thapar.edu>
2021-03-20 17:53:28 +0530

    Resources Model

--- demo/settings.py
+++ demo/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
-    'booking'
+    'booking',
+    'resources',
+    'user'
+
 ]
 
 MIDDLEWARE = [
